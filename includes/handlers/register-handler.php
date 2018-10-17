df4daaae7e107f551242c9656403308d3e520b17
Денис <dr.gribovitch@gmail.com>
2018-10-17 20:32:58 +0300

    End validate registr

--- includes/handlers/register-handler.php
+++ includes/handlers/register-handler.php
@@ -21,24 +21,19 @@ function sanitizeFormString($inputText)
   return $inputText;
 }
 
- if(isset($_POST['loginButton'])) {
-
- }
  if(isset($_POST['registerButton'])) {
   $username = sanitizeFormUsername($_POST['username']);
-
   $firstName = sanitizeFormString($_POST['firstName']);
-
   $lastName = sanitizeFormString($_POST['lastName']);
-
   $email = sanitizeFormString($_POST['email']);
-
   $email2 = sanitizeFormString($_POST['email2']);
-
   $password = sanitizeFormPassword($_POST['password']);
-  
   $password2 = sanitizeFormPassword($_POST['password2']); 
-
- }
+  
+  $wasSuccessful =  $account->register($username,$firstName,$lastName,$email,$email2,$password,$password2);
+  if($wasSuccessful == true) {
+    header("Location: index.php"); 
+  } 
+}
 
 ?>
--- register.php
+++ register.php
@@ -1,3 +1,11 @@
+<?php 
+ include("includes/classes/Account.php") ; 
+ $account = new Account();   
+ include("includes/handlers/register-handler.php") ;
+ include("includes/handlers/login-handler.php") ;
+  
+  
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -10,9 +18,9 @@
 <body>
    <div id="inputContainer">
 
-    <form id="loginForm" action="register.php" method="post">
+    <form id="loginForm" action="register.php" method="POST">
     <h2>Login to your account</h2>
-    <p>
+    <p>   
     <label for="loginUsername">Username</label>
     <input id="loginUsername" name="loginUsername" type="text" placeholder="Denis" required>
     </p>  
@@ -22,25 +30,30 @@
     </p>
     <button type="submit"name="loginButton">LOG IN</button> 
    </form>
-
-   <form id="registerForm" action="register.php" method="post">
-    <h2>Login to your account</h2>
+   
+   <form id="registerForm" action="register.php" method="POST">
+    <h2>Create your free account</h2> 
     <p>
+    <?php echo $account->getError("Your username must be between 5 and 25 characters"); ?>
     <label for="username">Username</label>
     <input id="username" name="username" type="text" placeholder="Denis" required>
     </p> 
 
-    <p>
+    <p>        
+    <?php echo $account->getError("Your first name must be between 2 and 25 characters"); ?>
     <label for="firstName">First name</label>
     <input id="firstName" name="firstName" type="text" placeholder="Hannibal" required>
     </p> 
 
     <p>
+    <?php echo $account->getError("Your last name must be between 2 and 25 characters"); ?>    
     <label for="lastName">Last name</label> 
     <input id="lastName" name="lastName" type="text" placeholder="GitHub" required>
     </p> 
 
     <p>
+    <?php echo $account->getError("Your emails don't match"); ?>
+    <?php echo $account->getError("Email is invalid"); ?>
     <label for="email">Email</label>
     <input id="email" name="email" type="email" placeholder="denis@gmail.com" required>
     </p> 
@@ -51,6 +64,9 @@
     </p> 
 
     <p> 
+    <?php echo $account->getError("Your password don't match"); ?>
+    <?php echo $account->getError("Your password can only contain numbers and letters"); ?>
+    <?php echo $account->getError("Your password must be between 2 and 30 characters"); ?>      
       <label for="password">Password</label>
      <input id="password" name="password" type="password" placeholder="Your password" required> 
     </p> 
