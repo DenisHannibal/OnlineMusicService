99838ea7087a8dbf1263e6bd46d1ee522e5d4516
Денис <dr.gribovitch@gmail.com>
2018-10-17 23:44:47 +0300

    Login BUG!!!!!!!!!!!

--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -9,6 +9,7 @@
     public static $firstNameCharacters = "Your first name must be between 2 and 25 characters";   
     public static $usernameCharacters = "Your username must be between 5 and 25 characters";      
     public static $usernameTaken = "This username already exists"; 
-    public static $emailTaken = "This email is already in use";   
+    public static $emailTaken = "This email is already in use"; 
+    public static $loginFailed = "You username or password was incorrect";   
   }
 ?>
